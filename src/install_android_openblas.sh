29a1f33e12ec2d3a66bfb8e481604a75d13d61ef
Kevin Yang <skyang@gmail.com>
2016-03-07 16:34:17 -0500

    Build Kaldi for android openblas
    	modified:   src/base/kaldi-math.cc
    	modified:   src/configure
    	new file:   src/install_android_openblas.sh
    	new file:   src/makefiles/android_openblas.mk
    	typechange: src/nnet2bin/raw-nnet-init
    	modified:   src/thread/Makefile

---
+++ src/install_android_openblas.sh
@@ -0,0 +1,25 @@
+# This assumes you've build the android toolchain, and that you want to build arm.
+# This will install to /opt/arm-tools
+
+TOOLCHAIN_PREFIX=${1:-/tmp/my-android-toolchain/bin/arm-linux-androideabi}
+INSTALL_PREFIX=${2:-/opt/arm-tools}
+TOOLCHAIN_INCLUDE=${3:-/tmp/my-android-toolchain/sysroot/usr/include/}
+OPENBLAS_ROOT=${4:-/opt/arm-tools}
+
+export TOOLCHAIN_PREFIX=$TOOLCHAIN_PREFIX
+export TOOLCHAIN_INCLUDE=$TOOLCHAIN_INCLUDE
+
+export CPP=${TOOLCHAIN_PREFIX}-cpp
+export AR=${TOOLCHAIN_PREFIX}-ar
+export AS=${TOOLCHAIN_PREFIX}-as
+export NM=${TOOLCHAIN_PREFIX}-nm
+export CC=${TOOLCHAIN_PREFIX}-g++
+export CXX=${TOOLCHAIN_PREFIX}-g++
+export LD=${TOOLCHAIN_PREFIX}-ld
+export RANLIB=${TOOLCHAIN_PREFIX}-ranlib
+
+./configure --android-openblas=yes --fst-root=/opt/arm-tools/ --openblas-root=$OPENBLAS_ROOT
+
+make -j7
+make install
+python ../install_kaldi.py . $INSTALL_PREFIX

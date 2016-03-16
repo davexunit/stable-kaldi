b785206cc79722104cf87585cc8533518c2469f9
Kevin Yang <skyang@gmail.com>
2016-03-16 12:00:24 -0400

    Change the way we build, add option to configure, add android_openblas makefile
    	modified:   configure
    	modified:   install_android_openblas.sh
    	new file:   makefiles/android_openblas.mk

--- src/install_android_openblas.sh
+++ src/install_android_openblas.sh
@@ -1,7 +1,13 @@
 # This assumes you've build the android toolchain, and that you want to build arm.
 # This will install to /opt/arm-tools
 
-export TOOLCHAIN_PREFIX=/tmp/my-android-toolchain/bin/arm-linux-androideabi
+TOOLCHAIN_PREFIX=${1:-/tmp/my-android-toolchain/bin/arm-linux-androideabi}
+INSTALL_PREFIX=${2:-/opt/arm-tools}
+TOOLCHAIN_INCLUDE=${3:-/tmp/my-android-toolchain/sysroot/usr/include/}
+OPENBLAS_ROOT=${4:-/opt/arm-tools}
+
+export TOOLCHAIN_PREFIX=$TOOLCHAIN_PREFIX
+export TOOLCHAIN_INCLUDE=$TOOLCHAIN_INCLUDE
 
 export CPP=${TOOLCHAIN_PREFIX}-cpp
 export AR=${TOOLCHAIN_PREFIX}-ar
@@ -12,19 +18,8 @@ export CXX=${TOOLCHAIN_PREFIX}-g++
 export LD=${TOOLCHAIN_PREFIX}-ld
 export RANLIB=${TOOLCHAIN_PREFIX}-ranlib
 
-./configure --mathlib=OPENBLAS --static --fst-root=/opt/arm-tools/ --openblas-root=/opt/arm-tools
-sed -i.bak 's/CXX = g++//g' ./kaldi.mk
-sed -i.bak 's/CC = $(CXX)//g' ./kaldi.mk
-sed -i.bak 's/RANLIB = ranlib//g' ./kaldi.mk
-sed -i.bak 's/AR = ar//g' ./kaldi.mk
-# Add some openblas flags to use hardfloat
-sed -i.bak 's/CXXFLAGS += -msse -msse2 / CXXFLAGS += -mhard-float -D_NDK_MATH_NO_SOFTFP=1 -I\/opt\/arm-tools\/include  -I\/tmp\/my-android-toolchain\/sysroot\/usr\/include /g' ./kaldi.mk
-sed -i.bak 's/-DHAVE_EXECINFO_H=1//g' ./kaldi.mk
-# Openblas flags for hardfloat
-sed -i.bak 's/LDFLAGS = -g/LDFLAGS = -Wl,--no-warn-mismatch -lm_hard -pie/g' ./kaldi.mk
-# android does not need pthread
-sed -i.bak 's/-lpthread//g' ./kaldi.mk
-sed -i.bak 's/-ldl -lm  -framework Accelerate/ \/opt\/arm-tools\/lib\/libopenblas.a \/opt\/arm-tools\/lib\/libclapack.a \/opt\/arm-tools\/lib\/liblapack.a \/opt\/arm-tools\/lib\/libblas.a \/opt\/arm-tools\/lib\/libf2c.a -ldl -lm_hard/g' ./kaldi.mk
-make -j7 online2bin
+./configure --android-openblas=yes --fst-root=/opt/arm-tools/ --openblas-root=$OPENBLAS_ROOT
+
+make -j7
 make install
-python ../install_kaldi.py . /opt/arm-tools/
+python ../install_kaldi.py . $INSTALL_PREFIX
